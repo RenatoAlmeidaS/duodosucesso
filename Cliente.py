ae54aa6622ba0c5a4264414635ad8931a0461cea
renatoalmeidas <renatodealmeida_9@hotmail.com>
2017-06-20 12:43:22 -0300

    Adicionado escolha de porta

--- Cliente.py
+++ Cliente.py
@@ -1,36 +1,44 @@
 import socket
-
-
-HOST = '127.0.0.1'     # Endereco IP do Servidor
-PORT = ler_porta()            # Porta que o Servidor esta
-
-
-
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-
-tcp.connect(dest)
-msg = raw_input()
-
-while msg <> '\x18':
-
-    tcp.send (msg)
-    msg = raw_input()
-
-tcp.close()
+import os
 
 def ler_porta():
 
-		porta = 0
-		escolha = 0
+	porta = 0
+	escolha = 0
 
-		while (escolha < 1 or escolha > 2):	
-			os.system("clear")	
-			escolha = raw_input("Porta servidor:\n1-Padrao\n2-Digitar porta\n")
+	while (escolha < 1 or escolha > 2):	
+		os.system("clear")	
+		escolha = input("Porta servidor:\n1-Padrao\n2-Digitar porta\n")
 
-		if (escolha == 1):
-			porta = 5555
-		else:
-			porta = raw_input("Digite a porta:\n")
+	if (escolha == 1):
+		porta = 5555
+	else:
+		porta = input("Digite a porta:\n")
 
-		return porta
+
+	return porta
+
+while (True):
+
+	PORT = ler_porta()				# Porta que o Servidor esta
+	HOST = '127.0.0.1'				# Endereco IP do Servidor
+
+	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	dest = (HOST, PORT)
+
+	try:
+		tcp.connect(dest)
+		pass
+	except Exception as e:
+		raw_input ("Falha no engano! Aperte uma tecla para tentar novamente!\n")
+		continue
+	os.system("clear")
+	msg = raw_input()
+
+
+	while msg <> '\x18':
+
+	    tcp.send (msg)
+	    msg = raw_input()
+
+	tcp.close()
