015fad17cb5e7d4bdd67c27c0ae622a0b07996ba
Bexiga <walacetales@hotmail.com>
2017-06-21 20:46:55 -0300

    Cliente so falta imprimir a resposta do servidor, mas ta tudo so o pipes

--- Cliente.py
+++ Cliente.py
@@ -1,50 +1,32 @@
 import socket
 import os
+import linecache
 
-def ler_porta():
+def ler_porta(id):
 
-	porta = 0
-	escolha = 0
-
-	while (escolha < 1 or escolha > 2):	
-		os.system("clear")	
-		escolha = input("Porta servidor:\n1-Padrao\n2-Digitar porta\n")
-
-	if (escolha == 1):
-		porta = 5555
-	else:
-		porta = input("Digite a porta:\n")
-
-
+	arq = open('portas', 'r')
+	porta = int(linecache.getline('portas', id))
+	arq.close()
+	print(porta)
 	return porta
 
-while (True):
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	PORT = ler_porta()				# Porta que o Servidor esta
-	HOST = '127.0.0.1'				# Endereco IP do Servidor
+try:
+	dest = ("localhost", ler_porta(1))
+	tcp.connect(dest)
+	pass
+except Exception as e:
+	raw_input ("Erro, pressione qualqer tecla para continuar\n")
 
-	tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	dest = (HOST, PORT)
+os.system("clear")
+print("Digite 'exit' para sair")
+msg = raw_input()
+#con, cliente = tcp.accept()
 
-	try:
-		tcp.connect(dest)
-		pass
-	except Exception as e:
-		raw_input ("Falha no engano! Aperte uma tecla para tentar novamente!\n")
-		continue
-	os.system("clear")
-	msg = raw_input()
-	#con, cliente = tcp.accept()
+while msg <> 'exit':
 
-	while msg <> 'exit':
+    tcp.send (msg)
+    msg = raw_input()
 
-	    tcp.send (msg)
-	    msg = raw_input()
-		#msg2 = con.recv(1024)
-		#if not msg2: break
-		#print (msg2)
-
-	tcp.close()
-	
-	if msg == 'exit':
-		break
+tcp.close()
