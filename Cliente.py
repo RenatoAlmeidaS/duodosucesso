50e923187251123a2928f0082d7b9430636fb37c
Nega <renatodealmeida_9@hotmail.com>
2017-06-22 16:51:20 -0300

    Loop adicionado em Catraca

--- Cliente.py
+++ Cliente.py
@@ -7,28 +7,29 @@ def ler_porta(id):
 	arq = open('portas', 'r')
 	porta = int(linecache.getline('portas', id))
 	arq.close()
-	print(porta)
+	#print(porta)
 	return porta
 def escolher_catraca():
 	return int(raw_input('Em qual RU quer entrar:\n'))
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while (True):
 
-try:
-	dest = ('localhost', ler_porta(escolher_catraca()))
-	tcp.connect(dest)
-	pass
-except Exception as e:
-	print("Erro: {}".format(e))
-	raw_input ('pressione qualqer tecla para continuar...\n')
+	try:
+		dest = ('localhost', ler_porta(escolher_catraca()))
+		tcp.connect(dest)
+		pass
+	except Exception as e:
+		print("Erro: {}".format(e))
+		raw_input ('pressione qualqer tecla para continuar...\n')
 
-os.system('clear')
-print('Digite "exit" para sair')
-msg = raw_input()
+	os.system('clear')
+	print('Digite "exit" para sair')
+	msg = raw_input()
 
-while msg <> 'exit':
+	while msg <> 'exit':
 
-    tcp.send (msg)
-    msg = raw_input()
+	    tcp.send (msg)
+	    msg = raw_input()
 
-tcp.send (msg)
-tcp.close()
+	tcp.send (msg)
+	tcp.close()
